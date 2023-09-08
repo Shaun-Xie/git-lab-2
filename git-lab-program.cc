7c79677eb452feca5b32b9a373b8b26aef47e2f5
Shaun <sx733322@ohio.edu>
2023-09-08 17:12:48 -0400

    lab 2 program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Shaun Xie
+ *        Date: 9/8/2023
+ * Description: cs2400 lab 2 program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+
+    return 0;
+}// main
